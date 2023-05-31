4614365379ff400d4eb1c8849e84be6ca9cd5629
derekirish3 <lcfisch2@asu.edu>
2023-05-31 11:56:31 -0700

    Fix: var name warning

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -7,7 +7,7 @@ fn main() {
             let code: &str = std::str::from_utf8(data).unwrap();
             let mut parser = Parser::new();
             parser.set_language(tree_sitter_go::language()).expect("Error loading Go grammar");
-            let parsed = parser.parse(code, None);
+            let _ = parser.parse(code, None);
         });
     }
 }
